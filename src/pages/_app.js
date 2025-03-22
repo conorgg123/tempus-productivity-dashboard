ac88712406c78f57d185acbde8ff6c8036a10d14
conorgg123 <conor.ghobrial@gmail.com>
2025-03-22 03:53:32 +0000

    🎨 Update project directory structure and add initial NextJS files with styling configuration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,18 @@
-import '../styles/globals.css'
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 // Add global polyfill at the top level
 if (typeof window !== 'undefined' && typeof global === 'undefined') {
   window.global = window;
 }
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp 
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+} 
