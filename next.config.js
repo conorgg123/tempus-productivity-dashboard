9f4539dcf8116fbe67654618dbd59644e9e11bf2
conorgg123 <conor.ghobrial@gmail.com>
2025-03-22 04:19:29 +0000

    🧪 Add end-to-end test configuration and setup Jest with custom matchers

--- next.config.js
+++ next.config.js
@@ -1,26 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: false,
-  images: {
-    unoptimized: true
-  },
-  distDir: 'out',
-  webpack: (config) => {
-    // Add Babel loader to use instead of SWC
-    config.module.rules.push({
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
-        }
-      }
-    });
-    
-    return config;
-  }
-}
-
-module.exports = nextConfig 
+} 
