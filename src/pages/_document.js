badb693757e042e535a609e1c75e4072705724a1
conorgg123 <conor.ghobrial@gmail.com>
2025-03-22 04:12:04 +0000

    🔧 Update project dependencies and configuration files to setup initial repo structure

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,28 +1,28 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="description" content="Productivity Dashboard Application" />
-          <script dangerouslySetInnerHTML={{
-            __html: `
-              // Fix for "global is not defined"
-              if (typeof global === "undefined") {
-                window.global = window;
-              }
-            `
-          }} />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
-}
-
-export default MyDocument 
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Productivity Dashboard Application" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            // Fix for "global is not defined"
+            if (typeof global === "undefined") {
+              window.global = window;
+            }
+          `
+        }} />
+        <link 
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
